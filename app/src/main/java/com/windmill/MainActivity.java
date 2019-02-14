773554678096ecfe0af933edd771987d1e80aa66
Harry <1515076700@qq.com>
2019-02-14 15:26:41 +0800

    实现基本功能

--- app/src/main/java/com/windmill/MainActivity.java
+++ app/src/main/java/com/windmill/MainActivity.java
@@ -2,12 +2,42 @@ package com.windmill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.windmillview.WindMillView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WindMillView mWindMill;
+    private Button mBtStart;
+    private Button mBtStop;
+    private WindMillView mWindMill2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWindMill = (WindMillView) findViewById(R.id.wm1);
+        mWindMill2 = (WindMillView) findViewById(R.id.wm2);
+        mBtStart = (Button) findViewById(R.id.bt_start);
+        mBtStop = (Button) findViewById(R.id.bt_stop);
+
+        mBtStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWindMill.startRotate();
+                mWindMill2.startRotate();
+            }
+        });
+
+        mBtStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWindMill.stop();
+                mWindMill2.stop();
+            }
+        });
     }
 }
